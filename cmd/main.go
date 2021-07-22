c96d6ff9ed42b2ea2269f5122cca143140b640df
paavo <shibaev.pp@phystech.edu>
2021-07-22 16:43:13 +0300

    finish

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ const (
 	postfix = "/chains/main/blocks/head"
 	local   = "http://localhost:8732" + postfix
 	remote  = "https://mainnet-tezos.giganode.io"
-	chat_id = 666
+	chat_id = 320767500
 )
 
 func main() {
